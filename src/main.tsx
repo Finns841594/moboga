bca4093014a0befd8c886f48a370e259e2da3ba7
Mariana <118630670+mariana-molina@users.noreply.github.com>
2023-04-02 00:53:24 +0200

    Update main.tsx
    Delete strictMode

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,13 @@ import './App.css';
 import './index.css';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<GoogleOAuthProvider
-			clientId={`608282841775-cr9vmcq2inkojijdi68ss3oe01q3vrvj.apps.googleusercontent.com`}
-		>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</GoogleOAuthProvider>
-	</React.StrictMode>
+	<GoogleOAuthProvider
+		clientId={`608282841775-cr9vmcq2inkojijdi68ss3oe01q3vrvj.apps.googleusercontent.com`}
+	>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</GoogleOAuthProvider>
 );
