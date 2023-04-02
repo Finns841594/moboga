8a6fc87f639c4a43b7ff4da8b6b19ed9ed006325
Feng Y <fengy142857@gmail.com>
2023-04-02 15:05:26 +0200

    test deployment

--- src/components/HeaderInDetails.tsx
+++ src/components/HeaderInDetails.tsx
@@ -2,10 +2,13 @@ import { useEffect, useRef, useState } from 'react';
 import useAuth from '../useAuth';
 import MenuBar from '../assets/menu-bar.svg';
 import { Dropdown } from './Dropdown';
+import { useParams } from 'react-router-dom';
 
-export const Header = () => {
+export const HeaderInDetails = () => {
 	const { authenticated, isAuthenticated, user } = useAuth();
 	const [divDrop, setDivDrop] = useState(false);
+	const params = useParams();
+
 	useEffect(() => {
 		isAuthenticated();
 	}, []);
@@ -16,6 +19,19 @@ export const Header = () => {
 				<h1 className="header-title">
 					<a href="/">Moboga</a>
 				</h1>
+
+				<div className='header_navbtns'>
+				<a href={'../books/' + params.id} className={params.media === 'movies' ? 'specialbtn' : 'bn5'}>
+					<p className="p">Movies</p>
+				</a>
+				<a href={'../books/' + params.id} className={params.media === 'books' ? 'specialbtn' : 'bn5'}>
+					<p className="p">Books</p>
+				</a>
+				<a href={'../games/' + params.id} className={params.media === 'games' ? 'specialbtn' : 'bn5'}>
+					<p className="p">Games</p>
+				</a>
+
+				</div>
 
 				{authenticated ? (
 					<div className="header-user-container">
